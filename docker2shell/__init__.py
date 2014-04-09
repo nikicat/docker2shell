21a595117ab3bdb62e325cb55fbb4f91077ab8b0
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-04-09 21:22:40 +0400

    added port publishing

--- docker2shell/__init__.py
+++ docker2shell/__init__.py
@@ -11,13 +11,15 @@ def cmdline(container):
     environment = []
     envs = ' '.join(['-e '+env for env in config['Env']])
     volumes = ' '.join(['-v {}:{}'.format(external, internal) for internal, external in cont['Volumes'].items()])
+    cont_ports = cont['HostConfig']['PortBindings'] or {}
+    ports = ' '.join(['-p {}:{}'.format(external[0]['HostPort'], internal[:-4]) for internal, external in cont_ports.items()])
     memory = config['Memory']
     cpu_shares = config['CpuShares']
     workdir = config['WorkingDir']
     cmd = ' '.join(config['Cmd'])
     image = config['Image']
 
-    return 'docker run {envs} {volumes} -m {memory} -c {cpu_shares} -w {workdir} -P --rm -t -i {image} {cmd}'.format(**locals())
+    return 'docker run {envs} {volumes} {ports} -m {memory} -c {cpu_shares} -w {workdir} --rm -t -i {image} {cmd}'.format(**locals())
 
 def main():
     argh.dispatch_command(cmdline)
